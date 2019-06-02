a6e217e99e99f0a246181cb476b693b0d866b296
Fernando Buccella <fer.buccella@gmail.com>
2019-06-02 20:45:01 -0300

    Finish verify function.

--- src/verify.js
+++ src/verify.js
@@ -10,13 +10,20 @@ const { InvalidJWTError } = require('./errors')
  * @param {String} token - The JSON web token.
  * @param {String} pem - The PEM encoded public RSA key.
  * @param {Object} [options = {}] - Additional options for jsonwebtoken's verify function.
+ * @param {Array<String>} [options.audience] - A set of valid values for the audience (aud) field.
+ * @param {String} [options.issuer] - A valid value for the issuer (iss) field.
  *
  * @returns {Promise<Object>} A promise that resolves to the decoded JWT payload if the verification
  * succeeds. Otherwise, it is rejected with the appropriate error.
  * */
 const verify = (token, pem, options = {}) => new Promise((resolve, reject) => {
-  // TODO: finish this with default options.
-  jwt.verify(token, pem, options, (err, payload) => {
+  const opts = Object.assign({}, options, {
+    algorithms: ['RS256'],
+    complete: false,
+    ignoreExpiration: false
+  })
+
+  jwt.verify(token, pem, opts, (err, payload) => {
     if (err) return reject(new InvalidJWTError(err.message))
     return resolve(payload)
   })
