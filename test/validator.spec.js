979794141d7605c756448724e39d11f030c6ef01
Fernando Buccella <fer.buccella@gmail.com>
2019-06-17 21:58:22 -0300

    Check init function is called only once when resolves.

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -251,6 +251,23 @@ describe('Validator', () => {
       expect(validator.pems).to.be.null
     })
 
-    it('Should return the same result if calling more than once and the promise is resolved')
+    it('Should return the same result if calling more than once and the promise is resolved', async () => {
+      const [jwk1, jwk2] = jwks
+      const validator = new AWSCognitoJWTValidator(config)
+      expect(validator.pems).to.be.null
+
+      // First call.
+      const firstScope = nock(validator.jwksUrl).get('').reply(httpStatus.OK, { keys: [jwk1] })
+      await expect(validator.init()).to.eventually.be.undefined
+      expect(firstScope.isDone()).to.be.true
+      expect(validator.pems).to.deep.equal({ [jwk1.kid]: pems[jwk1.kid] })
+      nock.cleanAll()
+
+      // Second call.
+      const secondScope = nock(validator.jwksUrl).get('').reply(httpStatus.OK, { keys: [jwk2] })
+      await expect(validator.init()).to.eventually.be.undefined
+      expect(secondScope.isDone()).to.be.false
+      expect(validator.pems).to.deep.equal({ [jwk1.kid]: pems[jwk1.kid] })
+    })
   })
 })
