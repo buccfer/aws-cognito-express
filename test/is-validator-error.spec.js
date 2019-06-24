16de7f0e46525caa57941dcf46771d9ad59f6ab7
Fernando Buccella <fer.buccella@gmail.com>
2019-06-24 19:18:03 -0300

    Add tests for isAWSCognitoJWTValidatorError function.

---
+++ test/is-validator-error.spec.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const { expect, chance } = require('./index')
+const { isAWSCognitoJWTValidatorError } = require('../src')
+const {
+  ConfigurationError, InitializationError, RefreshError, InvalidJWTError
+} = require('../src/errors')
+
+describe('Is Validator Error', () => {
+  it('Should return false if the parameter is NOT an error from this library', () => {
+    const err = chance.pickone([
+      chance.string(),
+      chance.integer(),
+      chance.floating(),
+      chance.bool(),
+      null,
+      undefined,
+      Symbol(chance.word()),
+      new Error('Unexpected error'),
+      { notAnError: true },
+      chance.n(chance.word, 5)
+    ])
+
+    expect(isAWSCognitoJWTValidatorError(err)).to.be.false
+  })
+
+  it('Should return true if the parameter is an error from this library', () => {
+    const joiErr = new Error()
+    joiErr.details = [{ message: chance.sentence() }, { message: chance.sentence() }]
+
+    const err = chance.pickone([
+      new ConfigurationError(joiErr),
+      new InitializationError(new Error('Error while initializing')),
+      new RefreshError(new InitializationError(new Error('Error while refreshing'))),
+      new InvalidJWTError('Invalid JWT')
+    ])
+
+    expect(isAWSCognitoJWTValidatorError(err)).to.be.true
+  })
+})
