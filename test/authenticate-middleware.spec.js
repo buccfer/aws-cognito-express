dfbd2b6d86c6fdace94f8d5446bd633d5314e335
Fernando Buccella <fer.buccella@gmail.com>
2019-06-30 12:43:01 -0300

    Authenticate middleware tests WIP.

--- test/authenticate-middleware.spec.js
+++ test/authenticate-middleware.spec.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const { expect } = require('./index')
-const { generateConfig } = require('./util')
+const { expect, sinon, chance } = require('./index')
+const { generateConfig, signToken } = require('./util')
 const { AUTHORIZATION_HEADER, AUTHENTICATION_SCHEME } = require('../src/constants')
 const { authenticate } = require('../src')
-const { ConfigurationError } = require('../src/errors')
+const { ConfigurationError, InvalidJWTError } = require('../src/errors')
 
-describe('Authenticate middleware', () => {
+describe('Authenticate', () => {
   it('Should throw ConfigurationError if config is invalid', () => {
     const config = generateConfig()
     Reflect.deleteProperty(config, 'userPoolId')
@@ -20,8 +20,45 @@ describe('Authenticate middleware', () => {
     expect(authenticateMiddleware.length).to.equal(3)
   })
 
-  it(`Should call next with an InvalidJWTError if ${AUTHORIZATION_HEADER} header is missing`)
-  it(`Should call next with an InvalidJWTError if authentication scheme is not ${AUTHENTICATION_SCHEME}`)
-  it('Should call next with the error if the token is invalid')
-  it('Should validate the token and set req.cognito with the JWT payload')
+  describe('Middleware', () => {
+    const res = {}
+    let config
+    let authenticateMiddleware
+    let req
+    let next
+    let tokenPayload
+
+    beforeEach(() => {
+      config = generateConfig()
+      authenticateMiddleware = authenticate(config)
+      next = sinon.spy()
+      tokenPayload = { email: chance.email(), email_verified: chance.bool() }
+    })
+
+    it(`Should call next with an InvalidJWTError if ${AUTHORIZATION_HEADER} header is missing`, async () => {
+      req = { header: name => undefined } // eslint-disable-line no-unused-vars
+      await authenticateMiddleware(req, res, next)
+      expect(next.calledOnce).to.be.true
+      const err = next.getCall(0).args[0]
+      expect(err).to.be.an.instanceOf(InvalidJWTError)
+      expect(err.message).to.equal(`Missing ${AUTHORIZATION_HEADER} header.`)
+    })
+
+    it(`Should call next with an InvalidJWTError if scheme is not ${AUTHENTICATION_SCHEME}`, async () => {
+      const token = signToken('key_1', tokenPayload, {
+        audience: chance.pickone(config.audience),
+        issuer: `https://cognito-idp.${config.region}.amazonaws.com/${config.userPoolId}`,
+        tokenUse: chance.pickone(config.tokenUse)
+      })
+      req = { header: name => `Basic ${token}` } // eslint-disable-line no-unused-vars
+      await authenticateMiddleware(req, res, next)
+      expect(next.calledOnce).to.be.true
+      const err = next.getCall(0).args[0]
+      expect(err).to.be.an.instanceOf(InvalidJWTError)
+      expect(err.message).to.equal('Invalid authentication scheme.')
+    })
+
+    it('Should call next with the error if the token is invalid')
+    it('Should validate the token and set req.cognito with the JWT payload')
+  })
 })
