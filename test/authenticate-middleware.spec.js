236c1501acc649c2b4d2ca19610894eedd4d9be9
Fernando Buccella <fer.buccella@gmail.com>
2019-06-30 12:01:27 -0300

    Add test to check authenticate middleware throws a ConfigurationError if the config is invalid.

--- test/authenticate-middleware.spec.js
+++ test/authenticate-middleware.spec.js
@@ -1,9 +1,18 @@
 'use strict'
 
+const { expect } = require('./index')
+const { generateConfig } = require('./util')
 const { AUTHORIZATION_HEADER, AUTHENTICATION_SCHEME } = require('../src/constants')
+const { authenticate } = require('../src')
+const { ConfigurationError } = require('../src/errors')
 
 describe('Authenticate middleware', () => {
-  it('Should throw ConfigurationError if config is invalid')
+  it('Should throw ConfigurationError if config is invalid', () => {
+    const config = generateConfig()
+    Reflect.deleteProperty(config, 'userPoolId')
+    expect(() => authenticate(config)).to.throw(ConfigurationError, /"userPoolId" is required/)
+  })
+
   it('Should return a middleware')
   it(`Should call next with an InvalidJWTError if ${AUTHORIZATION_HEADER} header is missing`)
   it(`Should call next with an InvalidJWTError if authentication scheme is not ${AUTHENTICATION_SCHEME}`)
