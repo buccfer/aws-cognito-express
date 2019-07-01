6ac7dc8fde0b3fadc61d6448560c2175c0cb2b6e
Fernando Buccella <fer.buccella@gmail.com>
2019-06-30 21:31:03 -0300

    Adjust jsdoc for verify-jwt.js file.

--- src/lib/verify-jwt.js
+++ src/lib/verify-jwt.js
@@ -5,16 +5,13 @@ const { InvalidJWTError } = require('./errors')
 
 /**
  * @private
- *
  * @description Verifies the JWT signature. If valid, it returns the decoded payload.
- *
  * @param {string} token - The JSON web token.
  * @param {string} pem - The PEM encoded public RSA key.
  * @param {Object} options - Additional fields to validate.
  * @param {string[]} options.audience - A set of valid values for the audience (aud) field.
  * @param {string} options.issuer - A valid value for the issuer (iss) field.
  * @param {string[]} options.tokenUse - A set of valid values for the token use (token_use) field.
- *
  * @returns {Promise<Object>} A promise that resolves to the decoded JWT payload if the verification
  * succeeds. Otherwise, it is rejected with the appropriate error.
  * */
